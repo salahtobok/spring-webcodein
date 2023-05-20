81d20fa172e7ee6ed80aafa32405f055f34ea6cf
salahtobok <salahtobok1@gmail.com>
2023-05-20 09:08:21 +0100

    First commit spring boot project test .

---
+++ src/main/java/com/admin/webcodein/WebcodeinApplication.java
@@ -0,0 +1,13 @@
+package com.admin.webcodein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcodeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcodeinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admin/webcodein/WebcodeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.webcodein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcodeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
