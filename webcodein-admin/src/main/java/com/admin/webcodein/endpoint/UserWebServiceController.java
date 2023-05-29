25d3a17ba804048e322ab8c0cd00ffb39973feb8
salahtobok <salahtobok1@gmail.com>
2023-05-29 11:04:05 +0100

    Update Webcodein Admin with spring using Spring . N°9 Details : 1 - Update database layer . 2 - Update app security by adding oauth

--- webcodein-admin/src/main/java/com/admin/webcodein/endpoint/UserWebServiceController.java
+++ webcodein-admin/src/main/java/com/admin/webcodein/endpoint/UserWebServiceController.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/ ")
 public class UserWebServiceController {
 
     private final UserDbService userDbService;
