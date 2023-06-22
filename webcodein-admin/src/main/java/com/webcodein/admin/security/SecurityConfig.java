4ee6947bf74248e77ab468407af6be03240a41d6
salahtobok <salahtobok1@gmail.com>
2023-06-22 19:03:26 +0100

    Update repo .

--- webcodein-admin/src/main/java/com/webcodein/admin/security/SecurityConfig.java
+++ webcodein-admin/src/main/java/com/webcodein/admin/security/SecurityConfig.java
@@ -1,61 +1,61 @@
-//package com.webcodein.admin.security;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.web.cors.CorsConfiguration;
-//
-//import java.util.List;
-//
-//@Configuration
-//@EnableWebSecurity
-//@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true")
-//@EnableMethodSecurity(jsr250Enabled = true)
-//@RequiredArgsConstructor
-//public class SecurityConfig {
-//
-//    private final JwtAuthConverter jwtAuthConverter;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.cors().configurationSource(request -> {
-//                    var cors = new CorsConfiguration();
-//                    cors.setAllowedOrigins(List.of("https://localhost:4200", "http://127.0.0.1:80", "http://example.com"));
-//                    cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
-//                    cors.setAllowedHeaders(List.of("*"));
-//                    return cors;
-//                }).and()
-//                .csrf()
-//                    .disable()
-//                .authorizeHttpRequests(registry -> {
-//                    try {
-//                        registry
-//                                .requestMatchers("/sayHelloApi").hasRole("USER")
-//                                .anyRequest().authenticated();
-//                                //.and().oauth2Login();
-//                    } catch (Exception e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                }
-//                )
-//                .oauth2ResourceServer()
-//                    .jwt()
-//                        .jwtAuthenticationConverter(jwtAuthConverter);
-//
-//
-//
-///*        http
-//                .sessionManagement()
-//                    .sessionCreationPolicy(STATELESS);*/
-//
-//
-//        return http.build();
-//    }
-//}
-//
-//
+package com.webcodein.admin.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true")
+@EnableMethodSecurity(jsr250Enabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> {
+                    var cors = new CorsConfiguration();
+                    cors.setAllowedOrigins(List.of("https://localhost:4200", "http://127.0.0.1:80", "http://example.com"));
+                    cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+                    cors.setAllowedHeaders(List.of("*"));
+                    return cors;
+                }).and()
+                .csrf()
+                    .disable()
+                .authorizeHttpRequests(registry -> {
+                    try {
+                        registry
+                                .requestMatchers("/sayHelloApi").hasRole("USER")
+                                .anyRequest().authenticated();
+                                //.and().oauth2Login();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                )
+                .oauth2ResourceServer()
+                    .jwt()
+                        .jwtAuthenticationConverter(jwtAuthConverter);
+
+
+
+/*        http
+                .sessionManagement()
+                    .sessionCreationPolicy(STATELESS);*/
+
+
+        return http.build();
+    }
+}
+
+
