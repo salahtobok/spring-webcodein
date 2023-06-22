bcc86c8eb00cf3e56dbe39ee86e22a2d36bd1cb5
salahtobok <salahtobok1@gmail.com>
2023-06-22 19:10:11 +0100

    Update repo .

--- webcodein-admin/src/main/java/com/webcodein/admin/security/SecurityConfig.java
+++ webcodein-admin/src/main/java/com/webcodein/admin/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors().configurationSource(request -> {
                     var cors = new CorsConfiguration();
-                    cors.setAllowedOrigins(List.of("https://localhost:4200", "http://127.0.0.1:80", "http://example.com"));
+                    cors.setAllowedOrigins(List.of("https://localhost:4200"));
                     cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
                     cors.setAllowedHeaders(List.of("*"));
                     return cors;
